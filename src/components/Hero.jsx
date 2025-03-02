19e32426f7dac3b0532dab7c4b0df958b7bf3eaa
huycls <hoanghuycls@gmail.com>
2025-03-02 09:33:20 +0700

    fix header class on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ function Hero() {
   ]
 
   return (
-    <div className="relative pt-16 sm:pt-20">
+    <div className="relative pt-20">
       <Swiper
         spaceBetween={0}
         centeredSlides={true}
